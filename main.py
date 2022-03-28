4beddad6e198f37fb51571a318bccae6e7c2f4e4
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-03-28 16:04:31 +0000

    first part of part A

--- main.py
+++ main.py
@@ -3,29 +3,13 @@ def seq3np1(n):
     count = 0
     while(n != 1):
         count = count + 1
-        if(n % 2) == 0:        
+        if(n % 2) == 0:       
             n = n // 2
-        else:               
+        else:                 
             n = n * 3 + 1
-    return count              
-
-seq3np1(2)
+    print(count)
 
 
 def main():
-  n = int(input("enter an upper bound: "))
-
-  while n > 0:
-    seq3np1(n)
-    if n < 0:
-      print("end program")
-      break 
-
-  for start in range(1, (n + 1)):
-    print(start)
-    currentstart = seq3np1(start)
-    print(currentstart)
-    
-  
-
+	seq3np1(5)
 main()
