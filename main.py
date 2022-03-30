2d1c9955b3c561b956007266a24e54fcec07e4e9
Caitlyn Maier <98415962+cmaier13@users.noreply.github.com>
2022-03-30 19:09:59 +0000

    more code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle 
 
 def seq3np1(n):
     count = 0
@@ -9,6 +10,29 @@ def seq3np1(n):
             n = n * 3 + 1
     print("Number of iterations: ", count)
 
+graphy = turtle.Turtle()
+data = turtle.Turtle()
+
+def lineGraph(myturtle=None, n=None):
+  graphy = turtle.Turtle()
+  data = turtle.Turtle()
+  wn = turtle.Screen()
+  turtle.setworldcoordinates(0,0,10,10)
+  max_so_far = 0
+
+  for i in range(1, n+1):
+    result = seq3np1(i)
+    if result > max_so_far:
+      max_so_far = result
+      set.worldcoordinates(0,0, i+10, max_so_far + 10)
+      data.goto(0, max_so_far)
+      data.write(str("Maximum so far: ", i, max_so_far), True, align="center")
+    else: 
+      max_so_far = max_so_far 
+    return max_so_far
+
+
+
 def main():
   n = int(input("Enter upper bound: "))
   while n < 0:
@@ -18,8 +42,9 @@ def main():
   for start in range(1, n+1):
     print("Current value: ", start)
     seq3np1(start)
-    
 
+  lineGraph(graphy, n)
+  
 
   
 main()
